fe441e2e3c0ed4e3391d4296f972bf0e12d70df6
Nwuba Chinonso Francis <nwubafrancism@gmail.com>
2026-04-05 10:00:29 +0100

    for loop example

---
+++ for_loop.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+students="Hunter Bobby Precious Sixtus Seun Francis"
+
+for student in ${students}
+#withouth variable for students in Hunter Bobby Precious Sixtus Seun Francis
+
+do
+	echo "Present: ${student}"
+	sleep 3
+done
+
+echo "Attendance Complete"
+
+echo "DONEEE"
+
+for number in {1..15}
+do
+	echo ${number}
+	sleep 3
+done
+
